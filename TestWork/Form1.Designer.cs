c9fe20c51f07d1e71b020c33a50b9967e3d320e6
Лейт <U17sagi@yandex.ru>
2017-01-20 16:46:14 +0300

    Проект обновлен. Убраны лишние файлы из проекта

---
+++ TestWork/Form1.Designer.cs
@@ -0,0 +1,230 @@
+﻿namespace TestWork
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.PlayButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureWin = new System.Windows.Forms.PictureBox();
+            this.pictureStep = new System.Windows.Forms.PictureBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.buttonUp = new System.Windows.Forms.Button();
+            this.buttonDown = new System.Windows.Forms.Button();
+            this.pictureCountHand = new System.Windows.Forms.PictureBox();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureWin)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureStep)).BeginInit();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureCountHand)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.panel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // PlayButton
+            // 
+            this.PlayButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.PlayButton.Font = new System.Drawing.Font("Segoe Print", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.PlayButton.ForeColor = System.Drawing.Color.DarkOliveGreen;
+            this.PlayButton.Location = new System.Drawing.Point(16, 6);
+            this.PlayButton.Name = "PlayButton";
+            this.PlayButton.Size = new System.Drawing.Size(90, 68);
+            this.PlayButton.TabIndex = 0;
+            this.PlayButton.Text = "Новая \r\nигра";
+            this.PlayButton.UseVisualStyleBackColor = true;
+            this.PlayButton.Click += new System.EventHandler(this.PlayButton_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Location = new System.Drawing.Point(13, 93);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Количество ручек";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.panel1.Controls.Add(this.pictureWin);
+            this.panel1.Controls.Add(this.pictureStep);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(474, 70);
+            this.panel1.TabIndex = 5;
+            // 
+            // pictureWin
+            // 
+            this.pictureWin.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureWin.BackColor = System.Drawing.Color.White;
+            this.pictureWin.Location = new System.Drawing.Point(336, 0);
+            this.pictureWin.Name = "pictureWin";
+            this.pictureWin.Size = new System.Drawing.Size(138, 70);
+            this.pictureWin.TabIndex = 8;
+            this.pictureWin.TabStop = false;
+            this.pictureWin.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureWin_Paint);
+            // 
+            // pictureStep
+            // 
+            this.pictureStep.BackColor = System.Drawing.Color.Beige;
+            this.pictureStep.Location = new System.Drawing.Point(12, 5);
+            this.pictureStep.Name = "pictureStep";
+            this.pictureStep.Size = new System.Drawing.Size(226, 60);
+            this.pictureStep.TabIndex = 1;
+            this.pictureStep.TabStop = false;
+            this.pictureStep.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureStep_Paint);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.DarkKhaki;
+            this.panel2.Controls.Add(this.buttonUp);
+            this.panel2.Controls.Add(this.buttonDown);
+            this.panel2.Controls.Add(this.pictureCountHand);
+            this.panel2.Controls.Add(this.PlayButton);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Right;
+            this.panel2.Location = new System.Drawing.Point(356, 70);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(118, 358);
+            this.panel2.TabIndex = 6;
+            // 
+            // buttonUp
+            // 
+            this.buttonUp.BackgroundImage = global::TestWork.Properties.Resources.up;
+            this.buttonUp.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.buttonUp.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonUp.Font = new System.Drawing.Font("Segoe Print", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonUp.ForeColor = System.Drawing.Color.DarkOliveGreen;
+            this.buttonUp.Location = new System.Drawing.Point(77, 109);
+            this.buttonUp.Name = "buttonUp";
+            this.buttonUp.Size = new System.Drawing.Size(20, 20);
+            this.buttonUp.TabIndex = 7;
+            this.buttonUp.UseVisualStyleBackColor = true;
+            this.buttonUp.Click += new System.EventHandler(this.buttonUp_Click);
+            // 
+            // buttonDown
+            // 
+            this.buttonDown.BackgroundImage = global::TestWork.Properties.Resources.dows;
+            this.buttonDown.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.buttonDown.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonDown.Font = new System.Drawing.Font("Segoe Print", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.buttonDown.ForeColor = System.Drawing.Color.DarkOliveGreen;
+            this.buttonDown.Location = new System.Drawing.Point(77, 130);
+            this.buttonDown.Name = "buttonDown";
+            this.buttonDown.Size = new System.Drawing.Size(20, 20);
+            this.buttonDown.TabIndex = 6;
+            this.buttonDown.UseVisualStyleBackColor = true;
+            this.buttonDown.Click += new System.EventHandler(this.buttonDown_Click);
+            // 
+            // pictureCountHand
+            // 
+            this.pictureCountHand.Location = new System.Drawing.Point(16, 109);
+            this.pictureCountHand.Name = "pictureCountHand";
+            this.pictureCountHand.Size = new System.Drawing.Size(51, 41);
+            this.pictureCountHand.TabIndex = 5;
+            this.pictureCountHand.TabStop = false;
+            this.pictureCountHand.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureCountHand_Paint);
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.BackColor = System.Drawing.Color.Beige;
+            this.pictureBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox.Location = new System.Drawing.Point(7, 7);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(342, 344);
+            this.pictureBox.TabIndex = 7;
+            this.pictureBox.TabStop = false;
+            this.pictureBox.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox_MouseClick);
+            this.pictureBox.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox_Paint);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 50;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.DarkKhaki;
+            this.panel3.Controls.Add(this.pictureBox);
+            this.panel3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel3.Location = new System.Drawing.Point(0, 70);
+            this.panel3.Name = "panel3";
+            this.panel3.Padding = new System.Windows.Forms.Padding(7);
+            this.panel3.Size = new System.Drawing.Size(356, 358);
+            this.panel3.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(474, 428);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.MaximumSize = new System.Drawing.Size(700, 600);
+            this.MinimumSize = new System.Drawing.Size(400, 300);
+            this.Name = "Form1";
+            this.Text = "Сейф братьев пилотов";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureWin)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureStep)).EndInit();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureCountHand)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.panel3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button PlayButton;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.PictureBox pictureStep;
+        private System.Windows.Forms.PictureBox pictureWin;
+        private System.Windows.Forms.PictureBox pictureCountHand;
+        private System.Windows.Forms.Button buttonDown;
+        private System.Windows.Forms.Button buttonUp;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Panel panel3;
+    }
+}
+
---
+++ TestWork/Form1.cs
@@ -0,0 +1,246 @@
+﻿using System;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace TestWork
+{
+    public partial class Form1 : Form
+    {
+        //Игровое поле
+        Field F;
+        //Строка результата
+        string win;
+        //флаг 
+        bool victory = true;
+        //размер поля
+        int countHand = 4;
+        //ширина ячейки 
+        public int PixW;
+        //высота ячейки
+        public int PixH;
+        //горизонтальная ручка
+        Image horizontal = Properties.Resources.left;
+        //вертикальная ручка
+        Image vertical = Properties.Resources.top;
+        //количество шагов которые сделал пользователь
+        public int step;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //при загрузке формы запускаем игру
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Init();
+        }
+
+        //запускает новую игру
+        private void PlayButton_Click(object sender, EventArgs e)
+        {
+            Init();
+        }
+
+        private void Init()
+        {
+            //проверяем, не запущен ли таймер
+            if (timer1.Enabled)
+            {
+                timer1.Stop();
+            }
+            //делаем инициализацию
+            victory = false;
+            win = ".. ^_^ ..";
+            int N = countHand;
+            PixW = pictureBox.Width / N;
+            PixH = pictureBox.Height / N;
+            //создаем объект поля
+            F = new Field(N);
+            //запускаем таймер, чтобы в нем можно было рисовать повороты ручек
+            timer1.Start();
+            //pictureBox.Invalidate();
+            pictureStep.Invalidate();
+            pictureWin.Invalidate();
+            pictureCountHand.Invalidate();
+        }
+
+        //поворачиваем ручку на поле
+        private void pictureBox_MouseClick(object sender, MouseEventArgs e)
+        {
+            //проверяем если объект существует, и мы только что не выиграли
+            if (F != null && !victory)
+            {
+                //делаем поворот ручки, индекс считаем через округление координат
+                bool rez = F.Rotation(e.Location.X / PixW, e.Location.Y / PixH);
+                //увеличиваем ход
+                step++;
+                //перерисовываем счетчик и поле
+                pictureBox.Invalidate();
+                pictureStep.Invalidate();
+                //если мы выиграли
+                if (rez)
+                {
+                    //помечаем
+                    win = "Победа!";
+                    victory = true;
+                }
+                //если еще не выиграли
+                else
+                {
+                    //тоже помечам
+                    win = "Мимо!";
+                }
+                //потом рисуем эту информацию
+                pictureWin.Invalidate();
+            }
+            
+        }
+
+        //если форма изменит размер, пересчитываем ячейки и делаем перерисовку
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            if (F != null)
+            {
+                PixH = pictureBox.Height / F.count;
+                PixW = pictureBox.Width / F.count;
+                pictureBox.Invalidate();
+            }
+        }
+
+        //вызываем перерисовку поля по таймеру
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            pictureBox.Invalidate();
+        }
+
+        //рисуем поле
+        private void pictureBox_Paint(object sender, PaintEventArgs e)
+        {
+            if (F != null) DrawField(e);
+        }
+
+        //поле
+        public void DrawField(PaintEventArgs e)
+        {
+            for (int i = 0; i < F.count; i++)
+            {
+                for (int j = 0; j < F.count; j++)
+                {
+                    //если ручку не трогали
+                    if (F.state[i][j] == 0)
+                    {
+                        //в зависимости от положения рисуем соответствующую картинку
+                        if (F.mas[i][j] == 0)
+                        {
+                            e.Graphics.DrawImage(horizontal, j * PixW, i * PixH + PixH / 4, PixW, PixH / 2);
+                        }
+                        else
+                        {
+                            e.Graphics.DrawImage(vertical, j * PixW + PixW / 4, i * PixH, PixW / 2, PixH);
+                        }
+                    }
+                    //если ручку трогали
+                    else
+                    {
+                        //сохраняем состояние Graphics
+                        var save = e.Graphics.Save();
+                        //смотрим в какую сторону надо поворачивать
+                        if (F.state[i][j] > 0)
+                        {
+                            //сдвигаем Graphics на середину подкартинки 
+                            e.Graphics.TranslateTransform(PixW * j + PixW / 2, PixH * i + PixH / 2);
+                            //Поворачиваем его на угол, зависящий от состояния
+                            e.Graphics.RotateTransform(9 * F.state[i][j]);
+                            //рисуем на нем картинку
+                            e.Graphics.DrawImage(horizontal, -PixW / 2, -PixH / 2 + PixH / 4, PixW, PixH / 2);
+                            //возращаемся к изначальному состоянию
+                            e.Graphics.Restore(save);
+                            //изменяем состояние
+                            F.state[i][j] -= 1f;
+                        }
+                        else
+                        {
+                            //аналогично, но поворот в другую сторону
+                            e.Graphics.TranslateTransform(PixW * j + PixW / 2, PixH * i + PixH / 2);
+                            e.Graphics.RotateTransform(-9 * F.state[i][j]);
+                            e.Graphics.DrawImage(vertical, -PixW / 2 + PixW / 4, -PixH / 2, PixW / 2, PixH);
+                            e.Graphics.Restore(save);
+                            F.state[i][j] += 1f;
+                        }
+                    }
+                }
+            }
+        }
+
+        //рисуем количество ходов
+        private void pictureStep_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.FillRectangle(new SolidBrush(Color.Beige), 1, 1, 224, 58);
+            e.Graphics.DrawRectangle(new Pen(Color.DarkGreen, 2), 1, 1, 224, 58);
+            e.Graphics.DrawString("Количество ходов - " + step.ToString(),
+                new Font("Segoe Print", 12),
+                new SolidBrush(Color.DarkGreen),
+                10,
+                14);
+        }
+
+        //рисуем сообщение о выигрыше или промахе
+        private void pictureWin_Paint(object sender, PaintEventArgs e)
+        {
+            SolidBrush sb;
+            if (F != null)
+            {
+                if (step % 2 == 0)
+                {
+                    sb = new SolidBrush(Color.DarkKhaki);
+                }
+                else
+                {
+                    sb = new SolidBrush(Color.DarkGreen);
+                }
+                e.Graphics.RotateTransform(30);
+                e.Graphics.DrawRectangle(new Pen(Color.DarkKhaki, 3), 20, -50, 120, 75);
+                e.Graphics.FillRectangle(new SolidBrush(Color.Beige), 20, -50, 120, 75);
+                e.Graphics.DrawString(win,
+                    new Font("Segoe Print", 14),
+                    sb,
+                    45,
+                    -25);
+            }
+        }
+
+        //рисуем размер матрицы
+        private void pictureCountHand_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.FillRectangle(new SolidBrush(Color.Beige), 1, 1, 49, 39);
+            e.Graphics.DrawRectangle(new Pen(Color.DarkGreen, 2), 1, 1, 49, 39);
+            e.Graphics.DrawString(countHand.ToString(),
+                new Font("Segoe Print", 12),
+                new SolidBrush(Color.DarkGreen),
+                10,
+                5);
+        }
+
+        //увеличиваем размер матрицы (countHand)
+        private void buttonUp_Click(object sender, EventArgs e)
+        {
+            if (countHand < 12)
+            {
+                countHand++;
+                pictureCountHand.Invalidate();
+            }
+        }
+
+        //уменьшаем размер матрицы (countHand)
+        private void buttonDown_Click(object sender, EventArgs e)
+        {
+            if (countHand > 2)
+            {
+                countHand--;
+                pictureCountHand.Invalidate();
+            }
+        }
+    }
+}
